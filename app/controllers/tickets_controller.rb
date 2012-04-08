bb535b9e4c8e7d988d7854bb28ef4f559b411ff1
dirk grandjean <dirkgrandjean@live.nl>
2012-04-08 21:00:02 +1200

    Implemented edit action for the tickets controller and taking Ryan Biggs feedback into account

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -6,44 +6,50 @@ before_filter :find_ticket, :only => [:show,
                                       :destroy]
                                      
 def new
-@ticket = @project.tickets.build	
+	@ticket = @project.tickets.build	
 end
+
 def create
-@ticket = @project.tickets.build(params[:ticket])
-if @ticket.save
-flash[:notice] = "Ticket has been created."
-redirect_to [@project, @ticket]
-else
-flash[:alert] = "Ticket has not been created."
-render :action => "new"
+	@ticket = @project.tickets.build(params[:ticket])
+           	if @ticket.save
+			flash[:notice] = "Ticket has been created."
+			redirect_to [@project, @ticket]
+		else
+			flash[:alert] = "Ticket has not been created."
+			render :action => "new"
+		end
 end
 
 def show
+	#empty because of find_ticket in before_filter
 end
 
 def edit
+	#empty because of find_ticket in before_filter
 end
 
 def update
+	#empty because of find_ticket in before_filter		
+		if @ticket.update_attributes(params[:ticket])
+			flash[:notice] = "Ticket has been updated."
+			redirect_to [@project, @ticket]
+		else
+			flash[:alert] = "Ticket has not been updated."
+			render :action => "edit"
+		end
+end
 
-if
-@ticket.update_attributes(params[:ticket])
-flash[:notice] = "Ticket has been updated."
-redirect_to [@project, @ticket]
-else
-flash[:alert] = "Ticket has not been updated."
-render :action => "edit"
-end
-end
 
-end
 
 private 
-   def find_project
+
+def find_project
         @project = Project.find(params[:project_id])
-   end
-def find_ticket
-@ticket = @project.tickets.find(params[:id])
 end
+
+def find_ticket
+	@ticket = @project.tickets.find(params[:id])
+end
+#end for class ending
 end
 
